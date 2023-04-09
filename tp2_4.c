7d4eac9ef5ee410fe19687d56f339b420372e173
VelizMiguelC <miniveliz14@gmail.com>
2023-04-09 10:07:27 -0300

    cambios

--- tp2_4.c
+++ tp2_4.c
@@ -16,15 +16,6 @@ void encontrarPCMayorVel(struct compu PC[], int tamanio);
 
 int main(){
     srand(time(NULL));
-   /*  struct compu PC;
-    char tipos[6][10]={"Intel" , "AMD", "Celeron", "Athlon", "Core", "Pentium"};
-    int indice_aleatorio = rand() % 6;
-    PC.velocidad = 1+rand()%3;
-    PC.anio = 2015+rand()%9;
-    PC.cantidad = 1+rand()%9;
-    PC.tipo_cpu = tipos[indice_aleatorio];
-    printf("%s", PC.tipo_cpu); 
- */
     struct compu maquinas[cant];
 
     for (int i = 0; i < cant; i++)
